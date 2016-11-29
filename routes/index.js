79014ce8518bf3f7994472c09f6416617efd1ac5
adeunifranz <adeunifranz@gmail.com>
2016-11-29 19:59:15 -0400

    proyecto reclamo 29/11/2016

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var reclamo = {
+	tipoReclamo:'ODECO', 
+	observacion: 'mal servicio', 
+	fechaReclamo:'29/11/2016'
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Avances en computacion' });
+  res.render('index', { title: 'Proyecto final', subtitle: 'Avances en computacion', reclamo:reclamo });
 });
 
 module.exports = router;
